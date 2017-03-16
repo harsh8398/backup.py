473ba74183b8b5ce495e55089a8432422821df10
Harsh Patel <pharsh58@gmail.com>
2017-03-16 15:22:47 +0530

    Tests improved

--- tests/pybackup_tests.py
+++ tests/pybackup_tests.py
@@ -1,22 +1,42 @@
 from nose.tools import *
+import os
 import pybackup
+src = '/home/harsh8398/Desktop/d1'
+dst = '/home/harsh8398/Desktop/d2'
 
 
 def test_getlist():
-    obj = pybackup.GetList('/home/harsh8398/Desktop/d1',
-                           '/home/harsh8398/Desktop/d2')
-    assert_equal(obj.src, '/home/harsh8398/Desktop/d1')
-    assert_equal(obj.dst, '/home/harsh8398/Desktop/d2')
-    assert_equal(obj.flist(), ['/home/harsh8398/Desktop/d1/Untitled Document'])
+    os.mkdir(src)
+    os.mkdir(dst)
+    f = open(os.path.join(src, 'test.txt'), 'w')
+    f.write("this is working")
+    f.close()
+    obj = pybackup.GetList(src, dst)
+    assert_equal(obj.src, src)
+    assert_equal(obj.dst, dst)
+    assert_equal(obj.flist(), ['/home/harsh8398/Desktop/d1/test.txt'])
+    os.remove(os.path.join(src, 'test.txt'))
+    os.rmdir(src)
+    os.rmdir(dst)
 
 
 def test_bckplist():
-    obj = pybackup.GetList('/home/harsh8398/Desktop/d1',
-                           '/home/harsh8398/Desktop/d2')
+    os.mkdir(src)
+    os.mkdir(dst)
+    f = open(os.path.join(src, 'test.txt'), 'w')
+    f.write("this is working")
+    f.close()
+    obj = pybackup.GetList(src, dst)
     mylist = obj.flist()
-    obj1 = pybackup.BckpList(obj.src, obj.dst)
-    assert_equal(obj1.src, '/home/harsh8398/Desktop/d1')
-    assert_equal(obj1.dst, '/home/harsh8398/Desktop/d2')
+    obj1 = pybackup.BckpList(src, dst)
+    assert_equal(obj1.src, src)
+    assert_equal(obj1.dst, dst)
     assert_equal(
         obj1.docopy(mylist),
-        '~ Copied -> /home/harsh8398/Desktop/d1/Untitled Document\n')
+        '~ Copied -> /home/harsh8398/Desktop/d1/test.txt\n')
+    f = open(os.path.join(dst, 'test.txt'), 'r')
+    assert_equal(f.readline().strip(), 'this is working')
+    os.remove(os.path.join(src, 'test.txt'))
+    os.remove(os.path.join(dst, 'test.txt'))
+    os.rmdir(src)
+    os.rmdir(dst)
