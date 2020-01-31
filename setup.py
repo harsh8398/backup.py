a223994a8bfb0b38c7eed993e7528a0835e5d917
Harsh Patel <pharsh58@gmail.com>
2020-02-01 00:07:15 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='backup.py',
-    version='0.1',
-    description='Recursive and Incremental backup utility',
-    url='http://github.com/harsh8398/backup.py',
+    version='0.1.0',
     author='Harsh Patel',
     author_email='pharsh58@gmail.com',
-    scripts=['bin/pybackup'],
+    description='Recursive and Incremental backup utility',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/harsh8398/backup.py',
     packages=['backup'],
+    scripts=['bin/pybackup'],
     data_files=[("", ["LICENSE.txt"])],
     install_requires=[
         "tqdm"
     ],
-    zip_safe=False)
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
